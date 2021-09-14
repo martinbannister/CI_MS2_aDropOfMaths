97fa7347cd5da7ee5221de33877524263d6baad9
martinbannister <martin.bannister91@gmail.com>
2021-09-14 20:30:24 +0100

    FEAT(game.js) add event handler to settings form

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,3 +2,6 @@ function playClick(e) {
     // Prevent default submit from firing
     e.preventDefault();
 }
+
+let settingsForm = document.getElementById('frmSettings');
+settingsForm.addEventListener('submit', playClick);
